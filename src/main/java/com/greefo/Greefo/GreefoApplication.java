81cb55048673cd25857f03eb7e84c09f28756c2a
sujit96-git <sujit07@gmail.com>
2024-10-05 15:53:28 +0530

    Greefo

---
+++ src/main/java/com/greefo/Greefo/GreefoApplication.java
@@ -0,0 +1,13 @@
+package com.greefo.Greefo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreefoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreefoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greefo/Greefo/GreefoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greefo.Greefo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreefoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
