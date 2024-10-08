7b7690b57264376dfc6e0c8997412539ed564b17
sdcodding <sdcodding@gmail.com>
2024-10-08 19:47:50 +0530

    task 2 changes

--- src/main/java/com/greefo/Greefo/daoImpl/ProductDaoImpl.java
+++ src/main/java/com/greefo/Greefo/daoImpl/ProductDaoImpl.java
@@ -1,5 +1,52 @@
 package com.greefo.Greefo.daoImpl;
 
-public class ProductDaoImpl {
+import java.util.ArrayList;
+
+import com.greefo.Greefo.dao.ProductDao;
+import com.greefo.Greefo.entity.Product;
+
+public class ProductDaoImpl implements ProductDao {
+
+	@Override
+	public ArrayList<Product> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductById(Product id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductByName(Product name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductByCategory(Product category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteProductByName(Product name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
