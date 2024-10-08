779c401ab42b560b3e9899e851faa01e35232468
sdcodding <sdcodding@gmail.com>
2024-10-08 20:19:26 +0530

    task2

---
+++ src/main/java/com/greefo/Greefo/serviceImpl/ProductServiceImpl.java
@@ -0,0 +1,52 @@
+package com.greefo.Greefo.serviceImpl;
+
+import java.util.ArrayList;
+
+import com.greefo.Greefo.entity.Product;
+import com.greefo.Greefo.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public ArrayList<Product> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductyId(Product id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductyName(Product name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductyCategory(Product category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteProductByName(Product names) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
